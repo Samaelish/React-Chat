81b48ee895f5fb967f82003183a79dd736eaf6fd
Samaelish <semnd007@gmail.com>
2021-04-15 23:10:44 +0300

    authentification phase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import { ChatEngine } from 'react-chat-engine'
+
+import ChatFeed from './components/ChatFeed'
+
+import './App.css'
 
 const App = () => {
     return (
-        <div>
-            
-        </div>
+        <ChatEngine 
+            height='100vh'
+            projectID='1934d4a6-3572-42c4-93a0-f0ae702ccf4d'
+            userName='Samael'
+            // userName='PublicUser'
+            userSecret='frontendChatReact358'
+            // userSecret='123123'
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
     )
 }
 
